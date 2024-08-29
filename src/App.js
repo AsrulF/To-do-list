76e38753a13d2509fa518b1ad43d37b879c3ea18
AsrulF <asrulfirdaus93@gmail.com>
2024-08-29 11:00:09 +0800

    Added Finished Project To-do List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './input.css';
+import ToDoHeader from './components/header';
+import TodoInput from './components/todo-input';
+import TodoList from './components/todo-list';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-screen h-screen bg-black flex items-center justify-center'>
+      <div className='bg-gray-300 w-[500px] h-[500px] rounded-xl p-3'>
+        <ToDoHeader />
+        <TodoInput />
+      </div>
     </div>
   );
 }
